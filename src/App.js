2f73be3dc2db11cb7a50b4e16464bbb542b3dc0c
geregr <rockyliu20110221@gmail.com>
2020-10-31 11:52:45 -0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    var name ="lol"
+    function his(){
+        alert("hey")
+    }
+
+    var style={
+        color:"red"
+    }
+
+    return (
+    <div style={style}>
+        <h1>{name}</h1>
+        <button onClick={his}>lol</button>
     </div>
-  );
+    )
 }
 
 export default App;
